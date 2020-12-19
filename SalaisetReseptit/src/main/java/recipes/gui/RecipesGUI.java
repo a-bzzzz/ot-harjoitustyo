4d2e214faac5460c9d4f6c5f5c4cf53070f8b30e
Asta Björk <asta.bjork@helsinki.fi>
2020-12-20 01:25:01 +0200

    turhat pois

--- SalaisetReseptit/src/main/java/recipes/gui/RecipesGUI.java
+++ SalaisetReseptit/src/main/java/recipes/gui/RecipesGUI.java
@@ -16,8 +16,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
